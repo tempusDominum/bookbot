a578243d89b1af23a9d9fbef5c0288295822ed1d
tempusDominum <leserra13@gmail.com>
2024-08-24 12:20:30 -0400

    reports word count and character count

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def main():
     book_path = "books.frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    character_count = count_characters(text)
-    print(character_count)
+    character_list = count_characters(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    for character, count in character_list:
+        print(f"The '{character}' character was found {count} times")
+    print("--- End report ---")
 
 def count_words(text):
     words = text.split()
@@ -13,8 +17,11 @@ def count_characters(text):
     character_count = {}
     lowered_text = text.lower()
     for character in lowered_text:
-        character_count[character] = character_count.get(character, 0) + 1 
-    return character_count
+        if character.isalpha():
+            character_count[character] = character_count.get(character, 0) + 1
+    character_list = list(character_count.items())
+    character_list.sort(reverse=True, key=lambda character_count: character_count[1])
+    return character_list
 
 def get_book_text(path):
     with open("books/frankenstein.txt") as f:
